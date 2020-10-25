1ea56b02fd80531678ff07988999c5d3aaf869f5
keypanic <kaigauscom@gmail.com>
2020-10-25 03:32:33 +0300

    'init'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if(!matrix) return [];
+  a = [];
+  matrix.forEach((array, i) => {
+    if(i%2)a.push(array.reverse()); 
+    else  a.push(array);
+  });
+  return [].concat(...a);
 }
